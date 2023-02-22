f609979b0210d97ccead3ba37afb4a153d24262f
Jungie29 <jungie.francisco@gmail.com>
2023-02-22 09:45:22 +0800

    essay text field
    pinalitan ko essay text field to white tas pina dark ko text para makita

--- admin/survey_report.php
+++ admin/survey_report.php
@@ -84,7 +84,7 @@ while($row=$answers->fetch_assoc()){
 									</div>
 									<div class="d-flex w-100">
 									<span class=""><?php echo isset($ans[$row['id']][$k]) ? count($ans[$row['id']][$k]) : 0 ?>/<?php echo $taken ?></span>
-									<div class="mx-1 col-sm-8"">
+									<div class="mx-1 col-sm-8">
 									<div class="progress w-100" >
 					                  <div class="progress-bar bg-primary progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $prog ?>%">
 					                    <span class="sr-only"><?php echo $prog ?>%</span>
@@ -97,7 +97,7 @@ while($row=$answers->fetch_assoc()){
 								<?php endforeach; ?>
 								</ul>
 						<?php else: ?>
-							<div class="d-block tfield-area w-100 bg-dark">
+							<div class="d-block tfield-area w-100 bg-light">
 								<?php if(isset($ans[$row['id']])): ?>
 								<?php foreach($ans[$row['id']] as $val): ?>
 								<blockquote class="text-dark"><?php echo $val ?></blockquote>
